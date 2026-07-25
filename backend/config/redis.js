f9513674a241e4dd9a76c627b73d9419a51c0096
AG24-hub <ghoshankita753@gmail.com>
2026-07-25 12:43:43 +0530

    minor changes for deplooyment

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -1,7 +1,7 @@
 const { createClient } = require("redis");
 
 const redisClient = createClient({
-    url: process.env.UPSTASH_REDIS_REST_URL
+    url: process.env.REDIS_URL,
 });
 
 redisClient.on("connect", () => {
